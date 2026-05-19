9417d91a47b47c39849b57ca08e1a79a552e4402
Nilesh <nilesh.yadav22@comp.sce.edu.in>
2026-05-19 18:09:09 +0530

    init commit

---
+++ src/test/java/com/Library/Management/System/ECommerceMultivendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Library.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceMultivendorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
